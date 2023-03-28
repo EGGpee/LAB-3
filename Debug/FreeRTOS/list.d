9664e27a56b3b218e1dc2b2b89db9aa442f2aa12
EGGpee <peter410171@gmail.com>
2023-03-28 14:36:22 +0800

    first commit

---
+++ Debug/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+FreeRTOS/list.o: ../FreeRTOS/list.c \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/projdefs.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/portable.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/list.h
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/FreeRTOS.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/projdefs.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/portable.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab0/FreeRTOS/include/list.h:
